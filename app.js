37c1431ca7281ed949e0df5b6441db03f03c75e2
Amandeep Kaur <amandeepk78btechcse2024@kccitm.edu.in>
2022-06-08 12:51:14 +0530

    07/06/22

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var logger = require(' morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -20,6 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/student', studentRouter)
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
