406888bd64b5d95b7a1a830cb599055280628655
Amandeep Kaur <amandeepk78btechcse2024@kccitm.edu.in>
2022-06-14 12:20:19 +0530

    jquery

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,38 @@
+
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
 
-/* GET home page. */
+var con = mysql.createConnection({
+  host: "easylearning.guru",
+  user: "kcc_student",
+  password: "Kccitm.edu.in1",
+  database: "kccStudent"
+});
+
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title:'Express' });
+    con.connect(function(err) {
+        if (err) console.log("err");
+        console.log("Connected!");
+      });
+    res.render('index')
 });
-router.get('/data', function(req, res, next) {
-  res.render('index', { title:'Express' });
+
+router.post('/about', function(req, res, next) {
+    console.log(req.body)
+    var sql = "INSERT INTO `amandeep` (`fname`, `lname`) \
+    VALUES ('"+req.body.fname+"', '"+req.body.lname+"');"
+    console.log(sql)
+   con.connect()
+      con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+
+      });
+    
+res.json({"Name":req.body})
 });
+
 
 module.exports = router;
